73e32c8f800af49a7280b155ba80535a6025feb3
ciprik13 <ciprianmoisenco.it@gmail.com>
2026-03-24 15:12:39 +0200

    Add TinaCMS build step with environment variables in deployment workflow

--- lab4/astro.config.mjs
+++ lab4/astro.config.mjs
@@ -1,11 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import tailwindcss from '@tailwindcss/vite';
 
-// https://astro.build/config
 export default defineConfig({
+  site: 'https://www.darion-nord.online',
+  base: '/',
   vite: {
-    plugins: [tailwindcss()]
-  }
+    plugins: [tailwindcss()],
+  },
 });
